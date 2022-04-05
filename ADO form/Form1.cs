5d6d7ab6c1235aee54823fe7bc80ccb70d85e65e
rvnagmode <101249215+rvnagmode@users.noreply.github.com>
2022-04-05 09:34:07 +0530

    Add project files.

---
+++ ADO form/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ADO_form
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+            con =new SqlConnection(@"Server=DESKTOP-HSLC884\SQLEXPRESS;Database=SqlDemoDB;Integrated Security=True");
+        }
+
+    }
+}
